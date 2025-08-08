b6ea0333c3329262d1f33956022cb545f2112d9a
LAPTOP-8C9L3FQT\Jitender chouhan <prakastiwarichs@gmail.com>
2025-08-08 16:26:01 +0530

    Updated something

--- src/app/Services/page.js
+++ src/app/Services/page.js
@@ -301,7 +301,15 @@ const faqs = [
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                 </svg>
-                <span>Book Appointment</span>
+                <a 
+                                        href="https://t.me/Tanuspa" 
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="bg-gradient-to-r from-blue-500 to-indigo-600 hover:from-blue-600 hover:to-indigo-700 text-white font-medium py-3 px-6 rounded-full flex items-center justify-center transition-all shadow-lg"
+                                      >
+                                        <FaTelegram className="text-xl mr-3" />
+                                        Meet Our Team
+                                      </a>
               </button>
             </div>
           </div>
